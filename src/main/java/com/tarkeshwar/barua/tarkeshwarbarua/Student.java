c1b0c886b92ad9674abd9dd869d226bd4b2bdef0
tbarua1 <tbarua1@gmail.com>
2020-10-23 05:21:23 +0000

    Create Student.java
    saved from GITHUB

---
+++ src/main/java/com/tarkeshwar/barua/tarkeshwarbarua/Student.java
@@ -0,0 +1,67 @@
+package com.tarkeshwar.barua.tarkeshwarbarua;
+
+import java.util.Objects;
+
+public class Student {
+    private long id;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    private String name,address,phone;
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(address, person.address) &&
+                Objects.equals(phone, person.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, phone);
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
