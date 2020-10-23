65dccffe9788c0274b6cbafe6e5a5d79fbc627ae
Tarkeshwar <tbarua1@gmail.com>
2020-10-23 04:53:41 +0000

    Commit From IDEA

--- src/main/java/com/tarkeshwar/barua/tarkeshwarbarua/Person.java
+++ src/main/java/com/tarkeshwar/barua/tarkeshwarbarua/Person.java
@@ -1,11 +1,40 @@
 package com.tarkeshwar.barua.tarkeshwarbarua;
 
+import java.util.Objects;
+
 public class Person {
     private long id;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
     private String name,address,phone;
 
     public long getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(address, person.address) &&
+                Objects.equals(phone, person.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, phone);
     }
 
     public void setId(long id) {
