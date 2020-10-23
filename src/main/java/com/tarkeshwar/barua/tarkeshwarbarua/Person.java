c69f7671f91468b87b7f18a9030ba56921d60c49
Tarkeshwar <tbarua1@gmail.com>
2020-10-23 04:48:08 +0000

    Commit From IDEA

---
+++ src/main/java/com/tarkeshwar/barua/tarkeshwarbarua/Person.java
@@ -0,0 +1,38 @@
+package com.tarkeshwar.barua.tarkeshwarbarua;
+
+public class Person {
+    private long id;
+    private String name,address,phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
